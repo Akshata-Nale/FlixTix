bafbbadd6520a45b3fc17cd40007b77c04084058
Akshata Nale <akshatanale111@gmail.com>
2023-04-24 20:39:15 +0530

    first commit

---
+++ src/main/java/com/example/BookMyShow/FlixTixApplication.java
@@ -0,0 +1,13 @@
+package com.example.BookMyShow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlixTixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlixTixApplication.class, args);
+	}
+
+}
